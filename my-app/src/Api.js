b16235990ed702b3a6da3c8b842a3eaa7aa91ad2
1ucky40nc3 <louisnwendler@gmail.com>
2022-09-16 12:55:25 +0200

    finished tutorial

--- my-app/src/Api.js
+++ my-app/src/Api.js
@@ -7,6 +7,25 @@ class App extends Component {
 
     componentDidMount() {
         const url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=Seona+Dancing&format=json&origin=*'
+        
+        fetch(url)
+            .then((result) => result.json())
+            .then((result) => {
+                this.setState({
+                    data: result,
+                })
+            })
+    }
 
+    render() {
+        const { data } = this.state
+
+        const result = data.map((entry, index) => {
+            return <li key={index}>{entry}</li>
+        })
+
+        return <ul>{result}</ul>
     }
 }
+
+export default App
