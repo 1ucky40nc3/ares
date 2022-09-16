1a561836c41cb5ad6b9157334079bfebd9ed8415
1ucky40nc3 <louisnwendler@gmail.com>
2022-09-16 13:12:16 +0200

    basic example

--- react-three-fiber/src/App.js
+++ react-three-fiber/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom'
+import { Canvas } from '@react-three/fiber'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="canvas-container">
+      <Canvas>
+        <ambientLight color={0x404040} intensity={0.1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+        <mesh>
+          <boxGeometry args={[2, 2, 2]} />
+          <meshStandardMaterial />
+        </mesh>
+      </Canvas>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
